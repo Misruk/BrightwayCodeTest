920fc228df6984c9fdec7e3081ebe56238f9ff5b
Tevin Meredith <tevin.meredith@gmail.com>
2022-04-17 23:42:54 -0400

    Updating currency text and display text.

--- Models/Trip.cs
+++ Models/Trip.cs
@@ -6,24 +6,25 @@ namespace BrightwayCodeTest.Models
 {
     public class Trip
     {
+        [Display(Name = "Invoice Number")]
         public int TripId { get; set; }
 
         [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$"), Required, Display(Name = "User Name")]
         public string? UserName { get; set; }
 
-        [Required, Display(Name = "Email")]
+        [RegularExpression(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$"), Required, Display(Name = "Email")]
         public string? UserEmail { get; set; }
 
-        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$"), Required]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$"), Required, Display(Name = "Travel Destination")]
         public string? Destination { get; set; }
 
-        [Range(0, 365), Required, Display(Name = "Duration of Trip (Days)")]
+        [Range(0, 365), Required, Display(Name = "Duration of Trip in Days")]
         public int Duration { get; set; }
 
         [Column(TypeName = "decimal(18, 2)"), Display(Name = "$ USD")]
         public decimal Currency { get; set; }
 
-        [Display(Name = "Converted Currency"), Column(TypeName = "decimal(18, 2)")]
+        [Display(Name = "Needed Native Currency"), Column(TypeName = "decimal(18, 2)")]
         public decimal ConvertedCurrency { get; set; }
     }
 }
